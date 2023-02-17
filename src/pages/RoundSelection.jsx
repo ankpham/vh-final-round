bc0dad10baeb2fca85b10480f45d5093f62966a9
An Pham <anpham@cttdvnatl.org>
2023-02-17 10:19:21 -0500

    updated 2nd and removed 3rd

--- src/pages/RoundSelection.jsx
+++ src/pages/RoundSelection.jsx
@@ -8,7 +8,7 @@ const RoundSelection = () => {
                 <div className='select-round'>
                     <Link className="link" to="/vl-selection">Vòng I</Link>
                     <Link className="link" to="/r2-home">Vòng II</Link>
-                    <Link className="link" to="/r3-home">Vòng III</Link>
+                    {/*<Link className="link" to="/r3-home">Vòng III</Link>*/}
                 </div>
             </div>
         </div>
