4d4343173b9f95db79f0c4befc91aea846c85ad2
An Pham <anpham@cttdvnatl.org>
2023-02-18 11:19:08 -0500

    styling changes

--- src/pages/VLSelection.jsx
+++ src/pages/VLSelection.jsx
@@ -30,7 +30,7 @@ const VLSelection = () => {
         <div className="selection">
             <div className="container category">
                 <Link className="link back-to-selection-right" to={"/selection"}>Trở Về Trang Đầu</Link> 
-                <h1 ref={heading} className='heading background-orange'>Vòng I</h1>
+                <h1 ref={heading} className='heading background-midnight-blue'>Vòng I</h1>
                 <div className='selection-container-category-points'>
                     <div ref={categoryElement} className="category">
                         <div className='column'>
